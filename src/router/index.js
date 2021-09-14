7011a19ce3362b8fe7e319088ff6fa2809f8bc04
poerevan9 <poerevan9@gmail.com>
2021-09-14 22:58:07 +0800

    video

--- src/router/index.js
+++ src/router/index.js
@@ -16,20 +16,17 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () =>
-      import("../views/About.vue"),
+    component: () => import("../views/About.vue"),
   },
   {
     path: "/work",
     name: "Work",
-    component: () =>
-      import("../views/Work.vue"),
+    component: () => import("../views/Work.vue"),
   },
   {
     path: "/blog",
     name: "Blog",
-    component: () =>
-      import("../views/Blog.vue"),
+    component: () => import("../views/Blog.vue"),
   },
 ];
 
--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,5 +1,35 @@
 <template>
   <div class="blog">
     <h1>This is a blog page</h1>
+    <video
+      controls
+      autoplay
+      webkit-playsinline
+      playsinline
+      loop
+      muted
+      ref="videoo"
+    >
+      <source
+        src="http://video.itunes.apple.com/apple-assets-us-std-000001/Video118/v4/67/76/0d/67760df3-65e6-a3d3-02a7-81340664bcf2/mzvf_5168019211393929040.720w.h264lc.U.p.m4v"
+      />
+    </video>
+    <button @click="onClick">+1</button>
   </div>
 </template>
+
+<script>
+export default {
+  mounted() {
+    const var123 = this.$refs.videoo;
+    var123.play();
+    var123.currentTime = 12;
+    console.log(this.$refs.videoo);
+  },
+  methods: {
+    onClick: function () {
+      this.$refs.videoo.currentTime += 1;
+    },
+  },
+};
+</script>
